b25b63858a4bb96a7f916517e7f2160d8732225e
Serhat Balık <mserhatbalik@gmail.com>
2022-01-31 19:16:29 +0300

    first commit

---
+++ vars/buildImage.groovy
@@ -0,0 +1,10 @@
+#!/usr/bin/env groovy
+
+def call() {
+    echo 'building the docker image..'
+    withCredentials([usernamePassword(credentialsId: 'nexus-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t 209.97.182.161:8083/my-latest-app:1.0 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin 209.97.182.161:8083"
+        sh 'docker push 209.97.182.161:8083/my-latest-app:1.0'
+    }
+}
