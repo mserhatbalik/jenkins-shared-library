a5232dbe1d939643c348e396c07096fd1fab386b
Serhat Balık <mserhatbalik@gmail.com>
2022-02-01 14:08:58 +0300

    created packages and implemented classes

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -12,7 +12,7 @@ class Docker implements Serializable {
 
     def buildDockerImage(String imageName, String repositoryIp) {
         script.echo 'building the docker image..'
-        script.withCredentials([usernamePassword(credentialsId: 'nexus-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        script.withCredentials([script.usernamePassword(credentialsId: 'nexus-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
             script.sh "docker build -t $imageName ."
             script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin $repositoryIp"
             script.sh "docker push $imageName"
