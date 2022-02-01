607c2b0ae568302937be9bbf49a741ddae9eb993
Serhat Balık <mserhatbalik@gmail.com>
2022-02-01 13:22:08 +0300

    updated nexus IP

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
 
-def call() {
+def call(String imageName) {
     echo 'building the docker image..'
     withCredentials([usernamePassword(credentialsId: 'nexus-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t 159.65.85.180:8083/my-latest-app:1.0 .'
+        sh "docker build -t $imageName ."
         sh "echo $PASS | docker login -u $USER --password-stdin 159.65.85.180:8083"
-        sh 'docker push 159.65.85.180:8083/my-latest-app:1.0'
+        sh "docker push $imageName"
     }
 }
